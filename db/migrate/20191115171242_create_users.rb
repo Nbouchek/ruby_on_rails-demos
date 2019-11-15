578b435f1893c0bf56699dcd6161390ac049b1a4
Nacer Bouchekhima <nbouchek@gmail.com>
2019-11-15 17:57:51 +0000

    Add another columns for created_at and updated_at.

--- db/migrate/20191115171242_create_users.rb
+++ db/migrate/20191115171242_create_users.rb
@@ -6,6 +6,10 @@ class CreateUsers < ActiveRecord::Migration[5.1]
       t.string "last_name", :limit => 50
       t.string "email", :default => '', :null => false
       t.string "password", :limit => 40
+
+      t.datetime "created_at"
+      t.datetime "updated_at"
+
       t.timestamps
     end
   end
