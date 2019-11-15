52fb19aa2d3aa55a50358374618b603a84edcf40
Nacer Bouchekhima <nbouchek@gmail.com>
2019-11-15 18:00:56 +0000

    id is included by default in rails. One way to remove it is to use :id => false

--- db/migrate/20191115171242_create_users.rb
+++ db/migrate/20191115171242_create_users.rb
@@ -1,6 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def up
-    create_table :users do |t|
+    create_table :users, :id => false do |t|
       t.column "first_name", :string, :limit => 25
       # the following is another way of creating a column of type string
       t.string "last_name", :limit => 50
