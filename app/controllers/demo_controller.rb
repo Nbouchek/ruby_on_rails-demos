541cd9c0e361877b512f367337ba1dc48b612996
Nacer Bouchekhima <nbouchek@gmail.com>
2019-11-15 14:17:39 +0000

    changing the default rendered template

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -3,8 +3,11 @@ class DemoController < ApplicationController
   layout false
 
   def index
+    # render default template /demo/index 
   end
 
   def hello
+    # chaning the default value: /demo/hello to  /demo/hello
+    render('index')
   end
 end
