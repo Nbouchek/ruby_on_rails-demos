1179aaca0f8c06d9bd0cc08ca76c22dc50307ed2
Nacer Bouchekhima <nbouchek@gmail.com>
2019-11-15 18:11:43 +0000

    db migration using:
    rails db:migrate

--- db/migrate/20191115171242_create_users.rb
+++ db/migrate/20191115171242_create_users.rb
@@ -1,16 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def up
-    create_table :users, :id => false do |t|
+    create_table :users do |t|
       t.column "first_name", :string, :limit => 25
-      # the following is another way of creating a column of type string
       t.string "last_name", :limit => 50
       t.string "email", :default => '', :null => false
       t.string "password", :limit => 40
 
-      # t.datetime "created_at"
-      # t.datetime "updated_at"
-
-      # this is a shorthand for "created_at" and "updated_at"
       t.timestamps
     end
   end
