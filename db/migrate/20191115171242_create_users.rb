59152182d90e625091b95374488f643bd73b411b
Nacer Bouchekhima <nbouchek@gmail.com>
2019-11-15 17:27:46 +0000

    Add limits to first_name and last_name columns

--- db/migrate/20191115171242_create_users.rb
+++ db/migrate/20191115171242_create_users.rb
@@ -1,10 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def up
     create_table :users do |t|
-      t.column "first_name", :string
+      t.column "first_name", :string, :limit => 25
       # the following is another way of creating a column of type string
-      t.string"last_name"
-      
+      t.string "last_name", :limit => 50
       t.timestamps
     end
   end
