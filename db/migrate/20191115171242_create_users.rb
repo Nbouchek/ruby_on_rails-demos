7f93b1606db6e6bf7b3ed2c01665f11e5faf0bcd
Nacer Bouchekhima <nbouchek@gmail.com>
2019-11-15 17:26:47 +0000

    Add first_name and last_name columns

--- db/migrate/20191115171242_create_users.rb
+++ db/migrate/20191115171242_create_users.rb
@@ -1,6 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def up
     create_table :users do |t|
+      t.column "first_name", :string
+      # the following is another way of creating a column of type string
+      t.string"last_name"
+      
       t.timestamps
     end
   end
