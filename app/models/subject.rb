25fef81bf31b9c7db6197cfdcebefe6262cfa7e9
Nacer Bouchekhima <nbouchek@gmail.com>
2019-11-16 13:38:58 +0000

    Implement association in rails: one-to-one relationship belongs_to ==> foreign key

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,5 +1,7 @@
 class Subject < ApplicationRecord
 
+  has_one :page
+
   scope :visible, -> { where(visible: true) }
   scope :invisible, -> { where(visible: false) }
   scope :sorted, -> { order('position ASC') }
