3c7afea2524f978da5a19799543450144532bfee
Nacer Bouchekhima <nbouchek@gmail.com>
2019-11-16 10:04:37 +0000

    Add columns to Subjects

--- db/migrate/20191116095638_create_subjects.rb
+++ db/migrate/20191116095638_create_subjects.rb
@@ -1,8 +1,17 @@
-class CreateSubjects < ActiveRecord::Migration[5.1]
-  def change
-    create_table :subjects do |t|
+# frozen_string_literal: true
 
+class CreateSubjects < ActiveRecord::Migration[5.1]
+  def up
+    create_table :subjects do |t|
+      t.string 'name'
+      t.string 'position'
+      t.string 'visible', default: false
+      t.string 'name'
       t.timestamps
     end
+  end
+
+  def down
+    drop_table :subjects
   end
 end
