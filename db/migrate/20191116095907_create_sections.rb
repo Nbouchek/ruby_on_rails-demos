94a91400f9f3f06775c3cc92db090442c8304f29
Nacer Bouchekhima <nbouchek@gmail.com>
2019-11-16 10:30:41 +0000

    Add columns to Section

--- db/migrate/20191116095907_create_sections.rb
+++ db/migrate/20191116095907_create_sections.rb
@@ -1,8 +1,23 @@
+# frozen_string_literal: true
+
 class CreateSections < ActiveRecord::Migration[5.1]
-  def change
+  def up
     create_table :sections do |t|
+      t.integer 'page_id'
+      t.string 'name'
+      t.integer 'position'
+      t.boolean 'visible', default: false
+      t.string 'content_type'
+      t.text 'content'
 
       t.timestamps
     end
+    # each foeign key has an index
+    # indexes makes search faster
+    add_index('sections', 'page_id')
+  end
+
+  def down
+    drop_table :sections
   end
 end
